595e531d58d355532c93f8a2f911dabdf6ef910a
CryptoFennec <suwichalala@gmail.com>
2024-12-24 18:02:44 +0700

    add scale factor for ETH -> USD

--- src/app/analytics/components/GlobalCharts.tsx
+++ src/app/analytics/components/GlobalCharts.tsx
@@ -13,15 +13,22 @@ interface ChartData {
     totalLiquidityETH: string
 }
 
+
+const PRICE_SCALING_FACTOR = 0.25;
+
 function CustomTooltip({ active, payload, label }: any) {
     if (active && payload && payload.length) {
         return (
             <div className="bg-white p-2 border border-gray-300 rounded shadow">
-                <p className="text-sm">{`Date: ${new Date(label).toLocaleDateString()}`}</p>
+                <p className="text-sm">{`Date: ${new Date(label).toLocaleDateString('en-US', {
+                    month: 'short',
+                    day: 'numeric',
+                    year: 'numeric',
+                })}`}</p>
                 <p className="text-sm font-bold">{`${payload[0].name.includes('volume') ? 'Volume' : 'Liquidity'}: ${payload[0].value.toLocaleString(undefined, {
                     minimumFractionDigits: 2,
                     maximumFractionDigits: 2
-                })} ${payload[0].name.includes('USD') ? 'USD' : 'WAN'}`}</p>
+                })} ${payload[0].name.includes('USD') ? 'USD' : 'USD'}`}</p>
             </div>
         )
     }
@@ -37,7 +44,7 @@ function ChartCard({ title, data, dataKey, color }: { title: string, data: any[]
             <CardContent className="px-2 py-2">
                 <div className="h-[300px]">
                     <ResponsiveContainer width="100%" height="100%">
-                        <LineChart data={data} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
+                        <LineChart data={data.map(entry => ({...entry, [dataKey]: entry[dataKey] * PRICE_SCALING_FACTOR}))} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
                             <XAxis
                                 dataKey="date"
                                 tickFormatter={(value) => new Date(value).toLocaleDateString('en-US', {
@@ -104,7 +111,7 @@ export default function GlobalCharts({ data }: { data: ChartData[] }) {
         color="#daff00" 
       /> */}
             <ChartCard
-                title="Daily Volume (WAN)"
+                title="Daily Volume (USD)"
                 data={chartData}
                 dataKey="volumeETH"
                 color="#daff00"
@@ -116,7 +123,7 @@ export default function GlobalCharts({ data }: { data: ChartData[] }) {
         color="#daff00" 
       /> */}
             <ChartCard
-                title="Total Liquidity (WAN)"
+                title="Total Liquidity (USD)"
                 data={chartData}
                 dataKey="liquidityETH"
 
